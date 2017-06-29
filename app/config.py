c4ab5791ae480c51573ec384c4e4dc497d8a1a6a
jonidajci33 <joni.dajci12@gmail.com>
2017-06-29 22:07:43 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG = True
-MYSQL_USER = 'kostandinang'
+MYSQL_USER = 'n2chasers'
 MYSQL_PASSWORD = 'b00kst0r3'
 MYSQL_DB = 'kostandinang$n2_bookstore'
 MYSQL_HOST = 'kostandinang.mysql.pythonanywhere-services.com'
